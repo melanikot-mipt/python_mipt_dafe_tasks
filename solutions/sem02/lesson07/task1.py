ea64b93b6e3fff80c0cdf3d4588d95f7c697d345
melanikot-mipt <kotova.mv@phystech.edu>
2026-04-10 10:19:41 +0300

    lesson07

---
+++ solutions/sem02/lesson07/task1.py
@@ -0,0 +1,113 @@
+from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use("ggplot")
+
+
+class ShapeMismatchError(Exception):
+    pass
+
+
+def set_style_for_violin(violin_parts_hor, violin_parts_vert):
+    for body in violin_parts_hor["bodies"]:
+            body.set_facecolor("lightpink")
+            body.set_edgecolor("hotpink")
+
+    for body in violin_parts_vert["bodies"]:
+        body.set_facecolor("lightpink")
+        body.set_edgecolor("hotpink")
+
+    for part in violin_parts_hor:
+        if part == "bodies":
+            continue
+
+        violin_parts_hor[part].set_edgecolor("hotpink")
+
+    for part in violin_parts_vert:
+        if part == "bodies":
+            continue
+
+        violin_parts_vert[part].set_edgecolor("hotpink")
+
+
+
+def visualize_diagrams(
+    abscissa: np.ndarray,
+    ordinates: np.ndarray,
+    diagram_type: Any,
+) -> None:
+    if abscissa.size != ordinates.size:
+        raise ShapeMismatchError
+    if diagram_type != "hist" and diagram_type != "violin" and diagram_type != "box":
+        raise ValueError
+    
+    figure = plt.figure(figsize=(8, 8))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    axis_scatter = figure.add_subplot(grid[:-1, :-1])
+    axis_diagram_vert = figure.add_subplot(
+        grid[:-1, 3],
+        sharey=axis_scatter,
+    )
+    axis_diagram_hor = figure.add_subplot(
+        grid[-1, :-1],
+        sharex=axis_scatter,
+    )
+
+    axis_scatter.scatter(abscissa, ordinates, color="hotpink", alpha=0.45)
+
+    if diagram_type == "hist":
+        axis_diagram_hor.hist(
+            abscissa,
+            bins=52,
+            color="lightpink",
+            density=True,
+            alpha=0.8,
+        )
+        axis_diagram_vert.hist(
+            ordinates,
+            bins=52,
+            color="lightpink",
+            orientation="horizontal",
+            density=True,
+            alpha=0.8,
+        )
+
+    if diagram_type == "violin":
+        violin_parts_hor = axis_diagram_hor.violinplot(abscissa, vert=False, showmedians=True)
+        violin_parts_vert = axis_diagram_vert.violinplot(ordinates, showmedians=True)
+
+        set_style_for_violin(violin_parts_hor, violin_parts_vert)
+
+    if diagram_type == "box":
+        axis_diagram_hor.boxplot(
+            abscissa,
+            vert=False,
+            patch_artist=True,
+            boxprops=dict(facecolor="lightpink"),
+            medianprops=dict(color="k"),
+        )
+        axis_diagram_vert.boxplot(
+            ordinates,
+            patch_artist=True,
+            boxprops=dict(facecolor="lightpink"),
+            medianprops=dict(color="k"),
+        )
+
+    axis_diagram_hor.invert_yaxis()
+
+    
+
+
+
+if __name__ == "__main__":
+    mean = [2, 3]
+    cov = [[1, 1], [1, 2]]
+    space = 0.2
+
+    abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
+
+    visualize_diagrams(abscissa, ordinates, "violin")
+    plt.show()
---
+++ solutions/sem02/lesson07/task2.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+plt.style.use("ggplot")
+
+with open('solutions\sem02\lesson07\data\medic_data.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+def medic_diagram(data):
+
+    labels, counts_before = np.unique(np.array(data["before"]), return_counts=True)
+    labels, counts_after = np.unique(np.array(data["after"]), return_counts=True)
+
+    figure, axis = plt.subplots(figsize=(8, 8))
+    width = 0.3
+
+    axis.bar(
+        np.arange(labels.size) - width/2,
+        counts_before,
+        width=width,
+        color="lightpink",
+        edgecolor="palevioletred",
+        label="before"
+    )
+
+    axis.bar(
+        np.arange(labels.size) + width/2,
+        counts_after,
+        width=width,
+        color="hotpink",
+        edgecolor="palevioletred",
+        label="after"
+    )
+
+    axis.set_title("Mitral disease stages", fontsize=17, fontweight="bold", c="orchid")
+    axis.set_xticks(np.arange(labels.size), labels=labels, weight="bold")
+    axis.set_ylabel("amount of people", fontsize=14, fontweight="bold", c="orchid")
+    axis.legend(fontsize=15)
+
+    figure.savefig("solutions/sem02/lesson07/medic_diagram.png", bbox_inches="tight")
+
+medic_diagram(data)
