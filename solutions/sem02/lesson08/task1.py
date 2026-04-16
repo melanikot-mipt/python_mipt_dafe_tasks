530f2f1db1672eaa6a891f1431aa7cb55334bc21
melanikot-mipt <kotova.mv@phystech.edu>
2026-04-16 20:04:28 +0300

    lesson08

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -1,34 +1,80 @@
-from functools import partial
-
 import matplotlib.pyplot as plt
 import numpy as np
-
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
 
+def signal_calculation(modulation, t, fc):
+    if not modulation:
+        return np.sin(2 * np.pi * fc * t)
+    return modulation(t) * np.sin(2 * np.pi * fc * t)
+
+
+def set_style(axis):
+    axis.set_title("Анимация сигнала", fontsize=17, fontweight="bold")
+    axis.set_xlabel("Время (с)", fontsize=12, fontweight="bold")
+    axis.set_ylabel("Амплитуда", fontsize=12, fontweight="bold")
+
+    axis.legend()
+
+
 def create_modulation_animation(
-    modulation, 
-    fc, 
-    num_frames, 
-    plot_duration, 
-    time_step=0.001, 
+    modulation,
+    fc,
+    num_frames,
+    plot_duration,
+    time_step=0.001,
     animation_step=0.01,
-    save_path=""
+    save_path="",
 ) -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    figure, axis = plt.subplots(figsize=(10, 8))
+
+    full_plot_duration = plot_duration + num_frames * animation_step
+    t_full = np.arange(0, full_plot_duration, time_step)
+    full_signal = signal_calculation(modulation, t_full, fc)
+
+    line, *_ = axis.plot([], [], c="darkmagenta", label="Сигнал")
+
+    def update_frame(frame_id):
+        start_time = frame_id * animation_step  # для текущего кадра
+        end_time = start_time + plot_duration
+
+        start_time_index = int(start_time / time_step)
+        end_time_index = start_time_index + int(plot_duration / time_step)
+
+        t = t_full[start_time_index:end_time_index]
+        signal = full_signal[start_time_index:end_time_index]
+
+        line.set_data(t, signal)
+        axis.set_xlim(start_time, end_time)
+        axis.set_ylim(2 * np.min(signal), 2 * np.max(signal))
+        return (line,)
+
+    set_style(axis)
+
+    animation = FuncAnimation(
+        figure,
+        update_frame,
+        frames=num_frames,
+        interval=animation_step,
+        blit=True,
+    )
+
+    if save_path:
+        animation.save(save_path, writer="pillow", fps=24)
+    return animation
 
 
 if __name__ == "__main__":
-    def modulation_function(t):
-        return np.cos(t * 6) 
 
-    num_frames = 100  
-    plot_duration = np.pi / 2 
-    time_step = 0.001  
-    animation_step = np.pi / 200 
-    fc = 50  
+    def modulation_function(t):
+        return np.cos(t * 6)
+
+    num_frames = 100
+    plot_duration = np.pi / 2
+    time_step = 0.001
+    animation_step = np.pi / 200
+    fc = 50
     save_path_with_modulation = "modulated_signal.gif"
 
     animation = create_modulation_animation(
@@ -38,6 +84,6 @@ if __name__ == "__main__":
         plot_duration=plot_duration,
         time_step=time_step,
         animation_step=animation_step,
-        save_path=save_path_with_modulation
+        save_path=save_path_with_modulation,
     )
     HTML(animation.to_jshtml())
--- solutions/sem02/lesson08/task2.py
+++ solutions/sem02/lesson08/task2.py
@@ -1,34 +1,135 @@
-from functools import partial
-
 import matplotlib.pyplot as plt
 import numpy as np
-
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
 
+def mark_cells(visited_cells, start_x, start_y):
+    if (
+        start_x < 0
+        or start_x >= visited_cells.shape[0]
+        or start_y < 0
+        or start_y >= visited_cells.shape[1]
+    ):
+        print("Начальная точка находится за пределами лабиринта")
+        return
+    if visited_cells[start_x, start_y] != -1:
+        print("Начальная точка находится в стене")
+        return
+
+    step = 0
+    cells = [(start_x, start_y, step)]
+    current = 0
+
+    while current < len(cells):
+        x, y, step = cells[current]
+        current += 1
+
+        # проверяем текущую клетку
+        if x < 0 or x >= visited_cells.shape[0] or y < 0 or y >= visited_cells.shape[1]:
+            continue
+
+        if visited_cells[x, y] != -1:
+            continue
+
+        visited_cells[x, y] = step
+
+        cells.append((x + 1, y, step + 1))
+        cells.append((x - 1, y, step + 1))
+        cells.append((x, y + 1, step + 1))
+        cells.append((x, y - 1, step + 1))
+
+
+def is_there_way_out(visited_cells, end):
+    if visited_cells[end] == -1:
+        print("Пути до выхода не существует")
+
+
+def image_maze(axis, maze):
+    x_size, y_size = maze.shape[1], maze.shape[0]
+    axis.set_title("Волновой алгоритм", fontsize=17, fontweight="bold")
+
+    axis.set_xticks(np.arange(0, x_size, max(1, x_size // 10)))
+    axis.set_yticks(np.arange(0, y_size, max(1, y_size // 10)))
+
+    axis.set_xticks(np.arange(-0.5, x_size, 1), minor=True)
+    axis.set_yticks(np.arange(-0.5, y_size, 1), minor=True)
+
+    axis.tick_params(axis="both", which="major", labelsize=12, width=3)
+
+    axis.grid(True, which="minor", linewidth=2, color="black", alpha=1)
+    axis.grid(False, which="major")
+
+    axis.set_xlim(-0.5, x_size - 0.5)
+    axis.set_ylim(y_size - 0.5, -0.5)
+
+    # окрашиваем путь лабиринта
+    way_mask = maze != 0
+    way = np.zeros((maze.shape[0], maze.shape[1], 4))
+    way[way_mask] = [0, 0, 0, 1]  # черный цвет для пути
+
+    axis.imshow(way, extent=[-0.5, x_size - 0.5, y_size - 0.5, -0.5], origin="upper", alpha=1)
 
 
 def animate_wave_algorithm(
-    maze: np.ndarray, 
-    start: tuple[int, int], 
-    end: tuple[int, int], 
-    save_path: str = ""
-)  -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    maze: np.ndarray, start: tuple[int, int], end: tuple[int, int], save_path: str = ""
+) -> FuncAnimation:
+    visited_cells = -1 * maze.astype(np.int32)
+
+    mark_cells(visited_cells, *start)
+    is_there_way_out(visited_cells, end)
+
+    figure, axis = plt.subplots(figsize=(10, 10))
+
+    m_size = 500 / max(*maze.shape)  # размер маркера
+    x_points, y_points = np.array([]), np.array([])
+    wave, *_ = axis.plot(x_points, y_points, "o", ms=m_size)
+    end_point, *_ = axis.plot([], [], "o", ms=m_size, c="green")
+
+    image_maze(axis, maze)  # изображаем сам лабиринт
+
+    def update(frame_id):
+        nonlocal x_points, y_points
+        if frame_id == 0:
+            x_points = np.append(x_points, start[1])
+            y_points = np.append(y_points, start[0])
+        else:
+            cells = np.where(visited_cells == frame_id)
+
+            new_x = cells[1]
+            new_y = cells[0]
+
+            x_points = np.append(x_points, new_x)
+            y_points = np.append(y_points, new_y)
+
+        if frame_id == visited_cells[*end]:
+            end_point.set_data([end[1]], [end[0]])
+        else:
+            end_point.set_data([], [])
+
+        wave.set_data(x_points, y_points)
+        return wave, end_point
+
+    animation = FuncAnimation(figure, update, frames=np.max(visited_cells) + 1, interval=200)
+
+    if save_path:
+        animation.save(save_path, writer="pillow", fps=3)
+    return animation
+
 
 if __name__ == "__main__":
     # Пример 1
-    maze = np.array([
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ])
+    maze = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
 
     start = (2, 0)
     end = (5, 0)
@@ -36,18 +137,16 @@ if __name__ == "__main__":
 
     animation = animate_wave_algorithm(maze, start, end, save_path)
     HTML(animation.to_jshtml())
-    
+
     # Пример 2
-    
-    maze_path = "./data/maze.npy"
+
+    maze_path = "solutions\sem02\lesson08\data\maze.npy"
     loaded_maze = np.load(maze_path)
 
     # можете поменять, если захотите запустить из других точек
-    start = (2, 0)
-    end = (5, 0)
+    start = (30, 4)
+    end = (100, 43)
     loaded_save_path = "loaded_labyrinth.gif"
 
     loaded_animation = animate_wave_algorithm(loaded_maze, start, end, loaded_save_path)
     HTML(loaded_animation.to_jshtml())
-    
-    
